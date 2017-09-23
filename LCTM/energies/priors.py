fixed a off-by-one bug in pairwise.py

LCTM/energies/priors.py
@@ -10,7 +10,7 @@ class CorePotential:
     def cost_fcn(self, model, X, Y):
         pass
     def compute(self, model, X, score):
-        pass                
+        pass
 
 # -----------------------------------------
 class class_prior(CorePotential):
@@ -34,7 +34,7 @@ def temporal_cost(cost, Yi, length):
     idxs = np.linspace(0, length-1, T).astype(np.int)
     for t in range(T):
         cost[Yi[t], idxs[t]] += 1
-    return cost    
+    return cost
 
 @jit("float64[:,:](float64[:,:], float64[:,:], int64)")
 def temporal_compute(score, ws, length):

LCTM/infer.py
@@ -117,7 +117,7 @@ def segmental_forward_normalized(x, max_segs, pw=None):
 	# Set nonzero entries to 0 for visualization
 	scores[np.isinf(scores)] = 0
 
-	return scores	
+	return scores
 
 
 def sparsify_incoming_pw(pw):

LCTM/learn.py
@@ -4,6 +4,11 @@ from copy import deepcopy
 
 from LCTM import ssvm
 from LCTM import utils
+import logging
+
+
+logger = logging.getLogger(__name__)
+
 
 def pretrain_weights(model, X, Y):
     # Take mean of all potentials
@@ -20,8 +25,8 @@ def pretrain_weights(model, X, Y):
     model.ws = costs
 
 
-def subgradient_descent(model, X, Y, n_iter=100, C=1., pretrain=True, verbose=True, 
-                        gradient_method="adagrad", learning_rate=0.1, decay_rate=.99, 
+def subgradient_descent(model, X, Y, n_iter=100, C=1., pretrain=True, verbose=True,
+                        gradient_method="adagrad", learning_rate=0.1, decay_rate=.99,
                         batch_size=5):
 
     if model.debug:
@@ -30,6 +35,8 @@ def subgradient_descent(model, X, Y, n_iter=100, C=1., pretrain=True, verbose=Tr
     n_samples = len(X)
 
     # Check that Xi is of size FxT
+    # FIXME: This is probably a bug
+    # if X[0].shape[0] > X[0].shape[1]:
     if X[0].shape[0] > X[0].shape[0]:
         X = [x.T for x in X]
     
@@ -47,46 +54,60 @@ def subgradient_descent(model, X, Y, n_iter=100, C=1., pretrain=True, verbose=Tr
             else:
                 pretrain_weights(model, X, Y)
 
-    
     costs_truth = [ssvm.compute_costs(model, X[i], Y[i]) for i in range(n_samples)]
     # print("Unaries costs", [c['unary'].sum() for c in costs_truth])
     cache = deepcopy(costs_truth[0]) * 0.
 
     for t in range(n_iter):
-        batch_samples = np.random.randint(0, n_samples, batch_size)
+        if gradient_method == "full gradient":
+            batch_samples = np.arange(0, n_samples)
+            batch_size = n_samples
+        else:
+            batch_samples = np.random.randint(0, n_samples, batch_size)
 
         # Compute gradient
-        w_diff = [ssvm.compute_ssvm_gradient(model, X[j], Y[j], costs_truth[j], C) for j in batch_samples]
-        w_diff = reduce(lambda x,y: x+y, w_diff)
+        #w_diff = [ssvm.compute_ssvm_gradient(model, X[j], Y[j], costs_truth[j], C) for j in batch_samples]
+        j = batch_samples[0]
+        x = X[j]
+        y = Y[j]
+        costs = costs_truth[j]
+        w_diff = ssvm.compute_ssvm_gradient(model, x, y, costs, C)
+        for j in batch_samples[1:]:
+            x = X[j]
+            y = Y[j]
+            costs = costs_truth[j]
+            w_diff += ssvm.compute_ssvm_gradient(model, x, y, costs, C)
+        #w_diff = reduce(lambda x,y: x+y, w_diff)
         w_diff /= batch_size
 
         # ===Weight Update===
         # Vanilla SGD
-        if gradient_method == "sgd":
+        if gradient_method == "sgd" or gradient_method == "full gradient":
             eta = learning_rate * (1 - t/n_iter)
-            w_diff = w_diff*eta
+            w_diff = w_diff * eta
         # Adagrad
         elif gradient_method == "adagrad":
-            cache += w_diff*w_diff
+            cache += w_diff * w_diff
             w_diff = w_diff / (cache + 1e-8).sqrt() * learning_rate
         # RMSProp
         elif gradient_method == "rmsprop":
             # cache = decay_rate*cache + (1-decay_rate)*w_diff.^2
             if t == 0:
-                cache += w_diff*w_diff
+                cache += w_diff * w_diff
             else:
                 cache *= decay_rate
-                cache += w_diff*w_diff*(1-decay_rate)
+                cache += w_diff * w_diff * (1-decay_rate)
             w_diff = w_diff / sqrt(cache + 1e-8) * learning_rate
         
         model.ws -= w_diff
 
         # Print and compute objective
-        if verbose and (t+1)%50==0:
+        if verbose and not (t+1) % 25:
             # sample_set = [5,18,22,34,7,23,15,16,9,28]
-            sample_set = list(range(5))
+            #sample_set = list(range(5))
+            sample_set = batch_samples
             objective_new = np.mean([model.objective(model.predict(X[i]), Y[i]) for i in sample_set])
-            print("Iter {}, obj={}".format(t+1, objective_new))
+            logger.debug("Iter {}, obj={}".format(t+1, objective_new))
             model.logger.objectives[t+1] = objective_new
 
 

LCTM/models.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 from collections import OrderedDict
 from functools import reduce
+import logging
 
 import sklearn.metrics as sm
 import scipy.ndimage as nd
@@ -17,10 +18,14 @@ from LCTM import learn
 from LCTM import ssvm
 
 
+logger = logging.getLogger(__name__)
+
+
 class Logger:
     # Log objective values for visualization
     def __init__(self):
         self.objectives = {}
+
 
 class CoreModel:
     name = ""
@@ -28,7 +33,7 @@ class CoreModel:
 
     # Model
     n_classes = None
-    n_features = None    
+    n_features = None
     inference_type = "framewise"
 
     # Learning
@@ -37,16 +42,16 @@ class CoreModel:
     objective = ssvm.objective_01
 
     # TODO: Add other regularlizers
-    regularizer = "" 
+    regularizer = ""
 
     def __init__(model, name="", debug=False, inference="framewise"):
         model.name = name
-        model.debug = debug        
+        model.debug = debug
         model.inference_type = inference
 
         model.potentials = OrderedDict()
         model.ws = weights.Weights()
-        model.logger = Logger()        
+        model.logger = Logger()
 
         assert inference in ["framewise", "segmental"], \
             "'inference' must be framewise or segmental"
@@ -166,7 +171,7 @@ class CoreModel:
                 else:
                     path = segmental_inference(score.T, model.max_segs, normalized=normalized)
 
-        return path 
+        return path
 
 
 class CoreLatentModel(CoreModel):
@@ -174,6 +179,7 @@ class CoreLatentModel(CoreModel):
     def __init__(self, n_latent, **kwargs):
         CoreModel.__init__(self, **kwargs)
         self.n_latent = n_latent
+
 
 class ChainModel(CoreModel):
     def __init__(self, skip=1, **kwargs):
@@ -210,6 +216,7 @@ class LatentChainModel(CoreLatentModel):
         # if skip: self.potentials["pw2"] = pw.pairwise("pw2", skip*2)
         if skip: self.potentials["pw"] = pw.pairwise(skip, name="pw")
 
+
 class LatentConvModel(CoreModel):
     def __init__(self, n_latent, conv_len=100, skip=1, prior=False, **kwargs):
         CoreLatentModel.__init__(self, n_latent, name="Latent Convolutional Model", **kwargs)
@@ -217,6 +224,7 @@ class LatentConvModel(CoreModel):
         if prior: self.potentials["temporal_prior"] = priors.temporal_prior(length=30)
         self.potentials["conv"] = unary.conv_unary(conv_len=conv_len)
         if skip: self.potentials["pw"] = pw.pairwise(skip, name="pw")
+
 
 class SegmentalModel(CoreModel):
     def __init__(self, pretrained=False, prior=0, pairwise=True, **kwargs):
@@ -226,6 +234,7 @@ class SegmentalModel(CoreModel):
         if pretrained: self.potentials["pre"] = unary.pretrained_unary()
         else: self.potentials["unary"] = unary.framewise_unary()
         if pairwise: self.potentials["seg_pw"] = pw.segmental_pairwise(name="seg_pw")
+
 
 class PretrainedModel(CoreModel):
     def __init__(self, skip=0, prior=0, segmental=False, **kwargs):

LCTM/ssvm.py
@@ -1,11 +1,17 @@
 import numpy as np
 from numba import jit, float64, int64
 from copy import deepcopy
+import logging
 
 from LCTM import weights
 
+
+logger = logging.getLogger(__name__)
+
+
 def hamming_loss(Yi, Y_truth):
     return np.sum(Yi!=Y_truth).astype(np.float)
+
 
 def objective_01(model, Yi, Y_truth):
     return np.sum(Yi!=Y_truth).astype(np.float)
@@ -20,8 +26,8 @@ def compute_costs(model, Xi, Yi):
         costs[key] += model.potentials[key].cost_fcn(model, Xi, Yi)
     return costs
 
+
 def compute_ssvm_gradient(model, Xi, Yi, cost_truth=None, C=1.):
-    
     # Predict states
     if model.is_latent:
         Zi = predict_best_latent(model, Xi, Yi)
@@ -38,9 +44,11 @@ def compute_ssvm_gradient(model, Xi, Yi, cost_truth=None, C=1.):
 
     return w_diff
 
+
 def reduce_latent_states(score, n_latent, n_classes):
     score = score.reshape([n_classes, n_latent, -1])
     return score.max(1)
+
 
 @jit("int64[:](float64[:,:], int64[:], int64)")
 def predict_best_latent_(score, Yi, n_latent):
@@ -54,6 +62,7 @@ def predict_best_latent_(score, Yi, n_latent):
         path[t] = Yi[t]*n_latent + best_latent
 
     return path
+
 
 def predict_best_latent(model, Xi, Yi):
     n_timesteps = Xi.shape[1]
@@ -77,6 +86,7 @@ def loss_augmented_unaries(score, Yi):
         score[Yi[t],t] -= 1
 
     return score
+
 
 @jit("float64[:,:](float64[:,:], int64[:], int64)")
 def latent_loss_augmented_unaries(score, Yi, n_latent):