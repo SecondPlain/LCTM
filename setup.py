46d3f3e6920d0ab3c8701d08de83d3f9f4789d80
Jonathan Jones <jdjones@jhu.edu>
2020-04-23 11:51:06 -0400

    removed ahead-of-time compilation from setup.py because it broke installation

--- setup.py
+++ setup.py
@@ -8,20 +8,18 @@ Colin Lea
 '''
 
 from distutils.core import setup
-from Cython.Distutils import build_ext
+# from Cython.Distutils import build_ext
 import numpy as np
 
 setup(
-	author = 'Colin Lea',
-	author_email = 'colincsl@gmail.com',
-	description = '',
-	license = "",
-	version= "0.1",
-	name = 'Latent Convolutional Timeseries Models',
-	cmdclass = {'build_ext': build_ext},
-	include_dirs = [np.get_include()],
-	packages= [	"LCTM",
-				"LCTM.energies",
-				],
-	# package_data={'':['*.xml', '*.png', '*.yml', '*.txt']},
+    author='Colin Lea',
+    author_email='colincsl@gmail.com',
+    description='',
+    license="",
+    version="0.1",
+    name='Latent Convolutional Timeseries Models',
+    # cmdclass={'build_ext': build_ext},
+    include_dirs=[np.get_include()],
+    packages=[	"LCTM", "LCTM.energies"],
+    # package_data={'':['*.xml', '*.png', '*.yml', '*.txt']},
 )
